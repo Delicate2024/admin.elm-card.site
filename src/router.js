fca367969e2131e507d7d3ad492138a7c168d7f2
ljx <ljxing_suda@163.com>
2025-05-13 13:17:28 +0800

    auto: update

--- src/router.js
+++ src/router.js
@@ -1,18 +1,8 @@
-// src/router.js
+// router.js
+
 import { createRouter, createWebHistory } from 'vue-router'
 import AdminLogin from './components/AdminLogin.vue'
 import Dashboard from './components/Dashboard.vue'
-import axios from 'axios'
-
-// 检查 JWT Token 是否有效
-const checkAuth = async () => {
-  try {
-    const response = await axios.get('/api/verifyToken', { withCredentials: true });
-    return response.data.success;
-  } catch (error) {
-    return false;
-  }
-}
 
 const routes = [
   { path: '/', component: AdminLogin },
@@ -24,19 +14,14 @@ const router = createRouter({
   routes,
 })
 
-router.beforeEach(async (to, from, next) => {
-  const isAuthenticated = await checkAuth();
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token'); // 或检查 cookie
 
-  // 如果访问的是登陆页，且已经登陆，跳转到 dashboard 页面
-  if (to.path === '/' && isAuthenticated) {
+  if (to.path === '/' && token) {
     next('/dashboard');
-  }
-  // 如果访问的是受保护的 dashboard 页面，但没有有效的 Token，跳转到登陆页
-  else if (to.path === '/dashboard' && !isAuthenticated) {
+  } else if (to.path === '/dashboard' && !token) {
     next('/');
-  }
-  // 默认情况下继续路由
-  else {
+  } else {
     next();
   }
 });
