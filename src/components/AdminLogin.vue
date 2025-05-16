af4fd2b1f87d9d8a6b7cfefa5ba46941044b26f5
ljx <ljxing_suda@163.com>
2025-05-16 23:00:42 +0800

    auto: update

--- src/components/AdminLogin.vue
+++ src/components/AdminLogin.vue
@@ -68,7 +68,7 @@ export default {
 		if (response.data.success) {
 		  console.log("Login successful, redirecting...");
 		  
-		  localStorage.setItem('username', username); 
+		  localStorage.setItem('username', this.username); 
 		  const redirectToken = response.data.redirectToken;
 		  localStorage.setItem('redirectToken', redirectToken);  
 		  const csrfToken = response.data.csrfToken;
