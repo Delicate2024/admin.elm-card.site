c4887e95f67c57645bf1f6dbde3c57579ef6c260
ljx <ljxing_suda@163.com>
2025-05-17 21:16:00 +0800

    auto: update

--- src/App.vue
+++ src/App.vue
@@ -8,26 +8,5 @@
 </script>
 
 <style scoped>
-@media screen and (orientation: portrait) {
-  body::before {
-    content: '请旋转手机横屏浏览';
-    position: fixed;
-    top: 0; left: 0;
-    width: 100vw;
-    height: 100vh;
-    background-color: black;
-    color: white;
-    font-size: 5vw;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    z-index: 9999;
-    text-align: center;
-    padding: 20px;
-  }
-
-  #app {
-    display: none;
-  }
-}
+/* 样式可保留或扩展 */
 </style>
